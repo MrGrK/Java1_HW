9b6f628cb26ce16c9f56285446cb2ae004fe1a03
MrGrK <grigksh@gmail.com>
2020-08-20 23:16:58 +0300

    Initial commit

---
+++ src/Java_1/Main.java
@@ -0,0 +1,18 @@
+package Java_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int xInt = 10000000;
+        byte xByte = 10;
+        long xLong = 10000000000l;
+        float xFloat = 1.999f;
+        double xDouble = 1.8888232323232;
+        boolean xBool = false;
+        char xCh = 's';
+        String xStr = "dsdasd";
+
+
+    }
+
+}
